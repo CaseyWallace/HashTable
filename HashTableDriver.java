cc1948f9def21a407ed83cc517bc5d153dd596b2
CaseyWallace <60989106+CaseyWallace@users.noreply.github.com>
2021-10-27 11:42:59 -0500

    Add files via upload

--- HashTableDriver.java
+++ HashTableDriver.java
@@ -1,3 +1,103 @@
-public class HashTableDriver {
-    
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class HashTableDriver{
+    public static void main(String args[]) {
+
+        //Initializes the MyHashTable and Array List to be added.  Tests the toAdd method.
+        
+        ICollection<Integer> ints = new MyHashTable<>();
+        Collection<Integer> toAdd = new ArrayList<Integer>();
+        toAdd.add(8);
+        toAdd.add(6);
+        toAdd.add(7);
+        toAdd.add(5);
+        toAdd.add(3);
+        toAdd.add(0);
+        toAdd.add(9);
+        ints.addAll(toAdd);
+        System.out.println("This is the initial MyHashTable: \n" + ints.toString());
+        System.out.println("The size of the MyHashTable is " + ints.size());
+        System.out.println("");
+        
+        //Tests adding a null value.
+
+        // ints.add(null);
+        // System.out.println("This is the MyHashTable after adding a null value. " + ints.toString());
+        // System.out.println("The size of the mtArrayList after adding one more element is " + ints.size());
+        // System.out.println("");
+        
+        //Tests the add method, and makes sure the size counter is updated.
+
+        ints.add(1000);
+        System.out.println("This is the MyHashTable after adding another value: \n" + ints.toString());
+        System.out.println("The size of the MyHashTable after adding one more element is " + ints.size());
+        System.out.println("");
+
+        //Tests the contains method if true.
+        if (ints.contains(7)) {
+            System.out.println("7 is in the MyHashTable!");
+        } else {
+            System.out.println("7 is not in the MyHashTable!");
+        }
+        System.out.println("");
+
+        //Tests the contains method if false.
+        if (ints.contains(81)) {
+            System.out.println("81 is in the MyHashTable!");
+        } else {
+            System.out.println("81 is not in the MyHashTable!");
+        }
+        System.out.println("");
+
+        //Tests the updateAll method
+        ints.add(7);
+        ints.updateAll(7, 100);
+        System.out.println("All instances of 7, including the one just added, have been updated to 100!: \n" + ints.toString());
+        System.out.println("");
+
+        //Tests the remove method.
+        ints.remove(3);
+        System.out.println("The first instance of 3 has been removed!: \n" + ints.toString());
+        System.out.println("");
+
+        //Tests the isEmpty method.
+        if (ints.isEmpty()){
+            System.out.println("The MyHashTable is empty!");
+        } else {
+            System.out.println("The MyHashTable is not empty!");
+        }
+        System.out.println("");
+
+        //Tests the clear method, then retests the isEmpty method.
+        ints.clear();
+        if (ints.isEmpty()){
+            System.out.println("The MyHashTable is empty!");
+        } else {
+            System.out.println("The MyHashTable is not empty!");
+        }
+        // System.out.println(ints.toString());
+        // System.out.println("");
+
+        //Creates movies to add to a new MyHashTable, adds the movies, and prints out the list of movies.
+        Movie a = new Movie("Ferris Bueller's Day Off", 1986);
+        Movie b = new Movie("Titanic", 1997);
+        Movie c = new Movie("Rocky", 1976);
+        Movie d = new Movie("Jaws", 1975);
+        // Movie e = null;
+
+        //Adds the created movies into the new MyHashTable object.
+        ICollection<Movie> movies = new MyHashTable<>();
+        movies.add(a);
+        movies.add(b);
+        movies.add(c);
+        movies.add(d);
+        // movies.add(e);
+
+        //Tests the Iterator
+        // System.out.println("The movies in the MyHashTable are:");
+        // for(var m : ArrayList) {
+        //     System.out.println(m);
+        // }
+    }
 }
--- MyHashTable.java
+++ MyHashTable.java
@@ -1,9 +1,18 @@
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.ArrayList;
 
 public class MyHashTable<T> implements ICollection<T> {
 
     private int size = 0;
+    private ArrayList[] table = new ArrayList[2];
+
+    //COMPLETE
+    public MyHashTable(){
+        for(int i = 0; i < table.length; i++) {
+            table[i] = new ArrayList();
+        }
+    }
 
     //COMPLETE
     @Override
@@ -11,28 +20,49 @@ public class MyHashTable<T> implements ICollection<T> {
         return new MyHashTableIterator(this);
     }
 
-    //TODO: Add in logic to add a new element.  If needed, expand the Hash Table.
+    //COMPLETE
     @Override
     public boolean add(T toAdd) {
-        return false;
+        boolean success = false;
+        int index = getIndex(toAdd);
+        table[index].add(toAdd);
+        size++;
+        success = true;
+        if (checkSize()) {
+            rehash(true);
+        }
+        return success;
     }
 
-    //TODO: Add in logic to add a full array of elements.
+    //COMPLETE
     @Override
     public boolean addAll(Collection<T> values) {
-        return false;
+        boolean success = false;
+        for(int i = 0; i < values.size(); i++) {
+            add((T) values.toArray()[i]);
+        }
+        success = true;
+        return success;
     }
 
-    //TODO: Add in logic to clear the Hash Table.
+    //COMPLETE
     @Override
     public void clear() {
-        
+        table = new ArrayList[2];
+        size = 0;
     }
 
-    //TODO: Add in logic to check if the Hash Table contains a value.
+    //COMPLETE
     @Override
     public boolean contains(T item) {
-        return false;
+        int index = getIndex(item);
+        boolean exists = false;
+        for(int i = 0; i < table[index].size(); i++) {
+            if (table[index].get(i).equals(item)) {
+                exists = true;
+            }
+        }
+        return exists;
     }
 
     //COMPLETE
@@ -41,16 +71,26 @@ public class MyHashTable<T> implements ICollection<T> {
         return (size == 0);
     }
 
-    //TODO: Add in logic to update all instances of a value to a new value.
+    //COMPLETE
     @Override
     public void updateAll(T oldValue, T newValue) {
-        
+        int index = getIndex(oldValue);
+        for(int i = 0; i < table[index].size(); i++){
+            if (table[index].get(i).equals(oldValue)){
+                table[index].set(i, newValue);
+            }
+        }
+        rehash(false);
     }
 
-    //TODO: Add in logic to remove an item.
+    //COMPLETE
     @Override
     public boolean remove(T item) {
-        return false;
+        boolean success = false;
+        int index = getIndex(item);
+        table[index].remove(item);
+        success = true;
+        return success;
     }
 
     //COMPLETE
@@ -59,9 +99,86 @@ public class MyHashTable<T> implements ICollection<T> {
         return size;
     }
 
-    //TODO: Add in logic to get an item at a given index.
-    public T get(int i) {
-        return null;
+    //COMPLETE
+    private int hash(T item) {
+        int returnValue;
+        if (item instanceof Integer){
+            returnValue = (int) item;
+        }
+        else {
+            returnValue = item.hashCode();
+        }
+        return returnValue;
     }
 
+    //COMPLETE
+    private void expand() {
+        table = new ArrayList[table.length * 2];
+        for(int i = 0; i < table.length; i++){
+            table[i] = new ArrayList();
+        }
+    }
+
+    //COMPLETE
+    private void rehash(boolean expands) {
+        ArrayList hold = new ArrayList();
+        for(int i = 0; i < table.length; i++) {
+            for(int j = 0; j < table[i].size(); j++) {
+                hold.add(table[i].get(j));
+            }
+        }
+        if (expands) {
+            size = 0;
+            expand();
+            for(int i = 0; i < hold.size(); i++){
+                add((T) hold.get(i));
+            }
+        }
+    }
+
+    //COMPLETE
+    private boolean checkSize(){
+        boolean needRehash = false;
+        for(int i = 0; i < table.length; i++) {
+            if (table[i].size() == table.length) {
+                needRehash = true;
+            }
+        }
+        return needRehash;
+    }
+
+    //COMPLETE
+    private int getIndex(T item){
+        return hash(item) % table.length;
+    }
+
+    @Override
+    public String toString(){
+        String returnString = "";
+        for(int i = 0; i < table.length; i++){
+            returnString += table[i].toString() + "\n";
+        }
+        return returnString;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public T get(int index){
+        T value = null;
+        int location = -1;
+        for (int i = 0; i < table.length; i++){
+            if (!table[i].isEmpty()){
+                for (int j = 0; j < table[i].size(); j++){
+                    value = (T) table[i].get(j);
+                    location++;
+                    if (location == index){
+                        break;
+                    }
+                }
+            }
+            if (location == index) {
+                break;
+            }
+        }
+        return value;
+    }
 }
